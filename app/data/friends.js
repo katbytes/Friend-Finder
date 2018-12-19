e8116face6814bd5041a035067c20031f56b7309
Katherine Bell <kbell@Katherine-MacBookPro.local>
2018-12-18 18:44:00 -0800

    fixed variable ids in json obj to reflect modal ids

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,51 @@
 var friends = [
   {
+    name: "Brad",
+    photo:
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/brad.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Mark",
+    photo:
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/eben.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Katherine",
+    photo:
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/kat.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Lily",
+    photo:
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/lily.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
     name: "Draggy",
-    photo: "/../assets/imgs/draggy.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    photo:
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/draggy.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
   {
     name: "Wolfgang",
     photo:
-      "/../assets/imgs/wolfgang.jpg",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/wolfgang.jpg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
   },
   {
     name: "Geppetto",
     photo:
-      "/../assets/imgs/geppetto.jpg",
-    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/geppetto.jpg",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Wolfgang & Geppetto",
+    photo:
+      "https://raw.githubusercontent.com/katbytes/Friend-Finder/master/app/assets/imgs/wolf_gepp.jpg",
+    scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
   }
 ];
 
